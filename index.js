357b33c1138bb4c130e367bfe3257013b6c62cbc
hamsame <hamsaaelmi@gmail.com>
2022-11-03 21:23:03 +0000

    Made static pages

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-app.get("/", (req, res) => {
-  res.send("home")
-})
-
-app.get("/about", (req, res) => {
-  res.send("about")
-})
-
-app.get("/*", (req, res) => {
-  res.send("404")
-})
+app.use(express.static("./public"))
 
 app.listen(port, () => `server is listening on port ${port}`)
